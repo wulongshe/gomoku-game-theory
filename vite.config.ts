d39abdff40263442affc4896a38c02d5c8ae5fc2
shewulong <shewulong@outlook.com>
2026-08-26 20:48:47 -0500

    server host

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,9 @@ import { cloudflare } from '@cloudflare/vite-plugin'
 import tailwindcss from '@tailwindcss/vite'
 
 export default defineConfig({
+  server: {
+    host: true,
+  },
   plugins: [vue(), cloudflare(), tailwindcss()],
   resolve: {
     alias: {
