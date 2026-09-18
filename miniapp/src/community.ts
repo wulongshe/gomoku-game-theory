30df20b700a3fbabe9e2cb1521e11e643f49dff8
shewulong <shewulong@outlook.com>
2026-09-17 23:25:15 -0500

    update group invite

--- miniapp/src/community.ts
+++ miniapp/src/community.ts
@@ -1,5 +1,5 @@
 export const GROUP_NAME = '博弈五子棋交流&组队'
-export const GROUP_INVITE = `9【长按复制这条邀请码消息，打开小红书即可加入该群】
-加入群聊"${GROUP_NAME}" CZ1510:/#q
-🍩😃🍣🍕🦊🍅
-🥮😗🐦🥐😷🐦`
+export const GROUP_INVITE = `6【长按复制这条邀请码消息，打开小红书即可加入该群】
+加入群聊"${GROUP_NAME}" CZ1510:/#y
+😋🥞🍇🐼😝🍔
+🍤🍖🐧😁😋😗`
